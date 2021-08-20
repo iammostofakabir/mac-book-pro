5d0a2406621adb047ee75e524420179a80f707c5
mostofakabir <76174777+mostofakabir@users.noreply.github.com>
2021-08-21 05:01:38 +0600

    002

--- script.js
+++ script.js
@@ -1,75 +1,98 @@
+// total price function
+
+function totalPrice() {
+    // totalPrice = basePrice + memoryCost + storageCost + deliveryCost
+    document.getElementById('totalPrice').innerText = parseFloat(document.getElementById('basePrice').innerText) + parseFloat(document.getElementById('memoryCost').innerText) + parseFloat(document.getElementById('storageCost').innerText) + parseFloat(document.getElementById('deliveryCost').innerText);
+    // update afterDiscount
+    document.getElementById('afterDiscount').innerText = document.getElementById('totalPrice').innerText;
+}
+
+
+// click button function
+
+function cickButton(btn1,btn2){
+    document.getElementById(btn).disabled = true;
+}
+
+
+
+// memory section
+
 document.getElementById('baseMemory').addEventListener('click', function () {
     document.getElementById('memoryCost').innerText = 0;
-    document.getElementById('totalPrice').innerText = parseFloat(document.getElementById('memoryCost').innerText) + 1299;
+    totalPrice();
 
-    // let button = document.getElementById('baseMemory');
-    // button.disabled = true;
-    // let button2 = document.getElementById('extendedMemory');
-    // button2.disabled = false;
+    document.getElementById('baseMemory').disabled = true;
+    document.getElementById('extendedMemory').disabled = false;
 });
+
 document.getElementById('extendedMemory').addEventListener('click', function () {
     document.getElementById('memoryCost').innerText = 180;
-    document.getElementById('totalPrice').innerText = parseFloat(document.getElementById('memoryCost').innerText) + 1299;
+    totalPrice();
 
-    let button = document.getElementById('baseMemory');
-    button.disabled = false;
-    let button2 = document.getElementById('extendedMemory');
-    button2.disabled = true;
+    document.getElementById('baseMemory').disabled = false;
+    document.getElementById('extendedMemory').disabled = true;
 });
 
 
+
+// storage section 
 
 document.getElementById('baseStorage').addEventListener('click', function () {
     document.getElementById('storageCost').innerText = 0;
-    document.getElementById('totalPrice').innerText = parseFloat(document.getElementById('storageCost').innerText) + 1299;
+    totalPrice();
 
-    let button = document.getElementById('baseStorage');
-    button.disabled = true;
-    let button2 = document.getElementById('extraStorage');
-    button2.disabled = false;
-    let button3 = document.getElementById('extendedStorage');
-    button3.disabled = false;
+    document.getElementById('baseStorage').disabled = true;
+    document.getElementById('extraStorage').disabled = false;
+    document.getElementById('extendedStorage').disabled = false;
 });
 document.getElementById('extraStorage').addEventListener('click', function () {
     document.getElementById('storageCost').innerText = 100;
-    document.getElementById('totalPrice').innerText = parseFloat(document.getElementById('storageCost').innerText) + 1299;
+    totalPrice();
 
-    let button = document.getElementById('baseStorage');
-    button.disabled = false;
-    let button2 = document.getElementById('extraStorage');
-    button2.disabled = true;
-    let button3 = document.getElementById('extendedStorage');
-    button3.disabled = false;
+    document.getElementById('baseStorage').disabled = false;
+    document.getElementById('extraStorage').disabled = true;
+    document.getElementById('extendedStorage').disabled = false;
 });
 document.getElementById('extendedStorage').addEventListener('click', function () {
     document.getElementById('storageCost').innerText = 180;
-    document.getElementById('totalPrice').innerText = parseFloat(document.getElementById('storageCost').innerText) + 1299;
+    totalPrice();
 
-    let button = document.getElementById('baseStorage');
-    button.disabled = false;
-    let button2 = document.getElementById('extraStorage');
-    button2.disabled = false;
-    let button3 = document.getElementById('extendedStorage');
-    button3.disabled = true;
+    document.getElementById('baseStorage').disabled = false;
+    document.getElementById('extraStorage').disabled = false;
+    document.getElementById('extendedStorage').disabled = true;
 });
 
 
+
+// delivery section
 
 document.getElementById('freeDelivery').addEventListener('click', function () {
     document.getElementById('deliveryCost').innerText = 0;
-    document.getElementById('totalPrice').innerText = parseFloat(document.getElementById('deliveryCost').innerText) + 1299;
+    totalPrice();
 
-    let button = document.getElementById('freeDelivery');
-    button.disabled = true;
-    let button2 = document.getElementById('expressDelivery');
-    button2.disabled = false;
+    document.getElementById('freeDelivery').disabled = true;
+    document.getElementById('expressDelivery').disabled = false;
 });
+
 document.getElementById('expressDelivery').addEventListener('click', function () {
     document.getElementById('deliveryCost').innerText = 20;
-    document.getElementById('totalPrice').innerText = parseFloat(document.getElementById('deliveryCost').innerText) + 1299;
+    totalPrice();
 
-    let button = document.getElementById('freeDelivery');
-    button.disabled = false;
-    let button2 = document.getElementById('expressDelivery');
-    button2.disabled = true;
+    document.getElementById('freeDelivery').disabled = false;
+    document.getElementById('expressDelivery').disabled = true;
 });
+
+
+
+// promo section
+
+document.getElementById('promoButton').addEventListener('click', function () {
+    if (document.getElementById('promoCode').value == 'stevekaku') {
+        document.getElementById('afterDiscount').innerText = parseFloat(document.getElementById('totalPrice').innerText) * 0.8;
+        document.getElementById('promoCode').value = '';
+    }
+    else {
+        document.getElementById('promoCode').value = '';
+    }
+})
